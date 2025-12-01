5c3fde69ef5774f437c49889a1078153927b4245
scheidt-morais <scheidt.morais@gmail.com>
2025-12-01 11:58:24 -0300

    Create updates.sql

---
+++ updates.sql
@@ -0,0 +1,12 @@
+
+UPDATE usuario
+SET telefone = '1191111-2222'
+WHERE nome = 'João Silva';
+
+UPDATE livro
+SET id_categoria = 2
+WHERE titulo = 'Dom Casmurro';
+
+UPDATE emprestimo
+SET data_prevista = '2025-02-20'
+WHERE id_emprestimo = 3;
